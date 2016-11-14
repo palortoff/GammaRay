a76275fe77fa53d0347cc0d6c3fc831edc3951ae
Volker Krause <volker.krause@kdab.com>
2016-11-14 12:01:16 +0100

    WIP Add QML binding inspector

--- plugins/qmlsupport/qmlsupport.cpp
+++ plugins/qmlsupport/qmlsupport.cpp
@@ -31,6 +31,7 @@
 #include "qmlattachedpropertyadaptor.h"
 #include "qjsvaluepropertyadaptor.h"
 #include "qmlcontextpropertyadaptor.h"
+#include "qmlbindingextension.h"
 #include "qmlcontextextension.h"
 #include "qmltypeextension.h"
 
@@ -345,6 +346,7 @@ QmlSupport::QmlSupport(GammaRay::ProbeInterface *probe, QObject *parent)
     PropertyAdaptorFactory::registerFactory(QJSValuePropertyAdaptorFactory::instance());
     PropertyAdaptorFactory::registerFactory(QmlContextPropertyAdaptorFactory::instance());
 
+    PropertyController::registerExtension<QmlBindingExtension>();
     PropertyController::registerExtension<QmlContextExtension>();
     PropertyController::registerExtension<QmlTypeExtension>();
 
